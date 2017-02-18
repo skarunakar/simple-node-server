ade1f1fafcb75846a3a3bf16aeb4487ec7f8055d
skarunakar <shreya_karunakar@intuit.com>
2017-02-18 20:32:13 +0530

    adding all records

--- server/server.js
+++ server/server.js
@@ -8,13 +8,27 @@ const hostname = '127.0.0.1';
 const port = 8000;
 
 const server = http.createServer((req, res) => {
+	var query = require('url').parse(req.url,true).query;
     res.statusCode = 200;
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader('Content-Type', 'application/json');
-    res.write(JSON.stringify(require( "./record1.json" )));
+    console.log(query);
+    res.write(JSON.stringify(require("./record"+query.dataSet+".json")));
     res.end();
 });
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+/*
+ [
+ '{{repeat(2, 100)}}',
+ {
+ _id: '{{repeat(integer(0, 100))}}',
+ name: '{{firstName()}} {{surname()}}',
+ gender: '{{gender()}}',
+ age: '{{integer(20, 40)}}',
+ marks: '{{integer(0, 100)}}'
+ }
+ ]*/
